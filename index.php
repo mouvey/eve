780b80456ea75b73b42c8242c267b194d2cba728
mouvey <vlynrafa@gmail.com>
2025-08-19 15:55:15 +0700

    php, date

--- index.php
+++ index.php
@@ -223,7 +223,7 @@ $data = mysqli_query($conn, "SELECT * FROM pertanyaan ORDER BY tanggal DESC");
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <div class="section-heading  wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
-            <h2> Certificates isra ganteng </h2>
+            <h2> Certificates </h2>
           </div>
         </div>
       </div>
